141bd2930af97b54bf3a33168ac47e7564d22237
Dave Grant <david@grant.org.uk>
2018-10-25 11:29:13 +0100

    Added tests to cover subscriber profile.

--- src/test/java/uk/org/grant/getkanban/SubscriberProfileTest.java
+++ src/test/java/uk/org/grant/getkanban/SubscriberProfileTest.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.assertThat;
 
 public class SubscriberProfileTest {
     @Test(expected = IllegalArgumentException.class)
-    public void testCannotFindSubscribersForInvalidCycleTime() {
+    public void profileMustBeGiven15Values() {
         SubscriberProfile profile = new SubscriberProfile(new int[] {});
         profile.getSubscribers(1);
     }
@@ -16,5 +16,17 @@ public class SubscriberProfileTest {
     public void testSubscribers() {
         SubscriberProfile profile = new SubscriberProfile(new int[]{20,10,0,0,0,0,0,0,0,0,0,0,0,0,0});
         assertThat(20, is(profile.getSubscribers(1)));
+    }
+
+    @Test
+    public void cycleTimeOver15ShouldReturnLastSubscriberNumber() {
+        SubscriberProfile profile = new SubscriberProfile(new int[]{20,10,0,0,0,0,0,0,0,0,0,0,0,0,20});
+        assertThat(20, is(profile.getSubscribers(100)));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void invalidCycleTimeShouldThrowException() {
+        SubscriberProfile profile = new SubscriberProfile(new int[]{20,10,0,0,0,0,0,0,0,0,0,0,0,0,20});
+        profile.getSubscribers(0);
     }
 }
