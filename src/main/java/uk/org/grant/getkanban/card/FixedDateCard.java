1ee85eaf30d341601004dda9656d422a26c47460
Dave Grant <david@grant.org.uk>
2018-11-12 22:16:34 +0000

    Remove unnecessary profile

--- src/main/java/uk/org/grant/getkanban/card/FixedDateCard.java
+++ src/main/java/uk/org/grant/getkanban/card/FixedDateCard.java
@@ -1,25 +1,15 @@
 package uk.org.grant.getkanban.card;
 
+import uk.org.grant.getkanban.Context;
 import uk.org.grant.getkanban.FixedSubscriberProfile;
 import uk.org.grant.getkanban.State;
 import uk.org.grant.getkanban.SubscriberProfile;
 
-public class FixedDateCard extends StandardCard {
+public class FixedDateCard extends AbstractCard {
     public FixedDateCard(String name, Size size, int analysis, int development, int test) {
-        super(name, size, analysis, development, test, new FixedSubscriberProfile());
+        super(name, size, analysis, development, test);
     }
 
-    public FixedDateCard(String name, Size size, int analysis, int development, int test, SubscriberProfile profile) {
-        super(name, size, analysis, development, test, profile);
-    }
-
-    public FixedDateCard(String name, Size size, int analysis, int development, int test, SubscriberProfile profile, int daySelected) {
-        super(name, size, analysis, development, test, profile, daySelected);
-    }
-
-    public FixedDateCard(String name, Size size, int analysis, int development, int test, SubscriberProfile profile, int daySelected, int dayDeployed) {
-        super(name, size, analysis, development, test, profile, daySelected, dayDeployed);
-    }
 
     @Override
     public boolean equals(Object o) {
@@ -37,5 +27,10 @@ public class FixedDateCard extends StandardCard {
     @Override
     public String toString() {
         return getName() + "[" + getRemainingWork(State.ANALYSIS) + "/" + getRemainingWork(State.DEVELOPMENT) + "/" + getRemainingWork(State.TEST) + "]" ;
+    }
+
+    @Override
+    public int getSubscribers() {
+        return 0;
     }
 }
